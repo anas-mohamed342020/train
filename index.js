b8d6b979377fb545827037fa824e6aa10d02d7f1
anas-mohamed342020 <ans192174@fci.bu.edu.eg>
2023-04-30 16:38:51 +0300

    new test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ config({ path: path.resolve('config/.env') })
 
 const port = process.env.port || 5000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Anas Anas'))
 app.get('/send',(req,res)=>{
     res.json(req.query) 
     // anas
