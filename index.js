2001b2436fcc4f0ae036499e52b0925316893679
anas-mohamed342020 <ans192174@fci.bu.edu.eg>
2023-04-30 16:32:27 +0300

    q

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import { config } from 'dotenv'
 import path from 'path'
 
 config({ path: path.resolve('config/.env') })
+
 const port = process.env.port || 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/send',(req,res)=>{
+    res.json(req.query)
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
